ac320cd0ab1a803f7b81b1a4c344a8295a3d2e58
Pinku-Neko <henryiakvictor@gmail.com>
2023-10-15 00:35:13 +0200

    init applications

---
+++ modules/application/generation.py
@@ -0,0 +1,7 @@
+'''
+generate images using normal p_sample and with predict
+'''
+
+
+def generate_image(diffusion, regression=None):
+    pass
---
+++ modules/application/resotration.py
@@ -0,0 +1,7 @@
+'''
+restore noisy image based on models
+'''
+
+
+def restore(image, diffusion, regression):
+    pass
