1b05ec1b02ba19be650e9159a8fdf6dec52fc416
Pinku-Neko <henryiakvictor@gmail.com>
2023-10-17 12:32:59 +0200

    try to fix stuff

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from modules.models.tools import init_models
 from modules.models.IO import load_model
 from modules.models.test import evaluate_regression
 from modules.train.MLP import train_MLP
-from modules.train.ddpm import train_diffusion
+from modules.train.ddpm import train_diffusion, test_power_training
 from modules.applications.test import get_image
 import modules.images.tools as imgtools
 import modules.applications.generation as gen
@@ -16,19 +16,19 @@ def main():
     # get previous diffusion model
 
     # filename diffusion
-    diffusion_name = "./saved_models/diffusion_best_0.0001.pt"
+    diffusion_name = "./saved_models/diffusion_best_1e-05.pt"
 
     # filename regression
     regression_name = "./saved_models/regression_best_256_0.0001.pt"
 
     # load models
-    diffusion, _ = init_models(regression_layer_dim=128)
-    diffusion = load_model(diffusion, diffusion_name)
-    # regression = load_model(regression, regression_name)
+    # diffusion, _ = init_models(regression_layer_dim=128)
+    # diffusion = load_model(diffusion, diffusion_name)
+    # # regression = load_model(regression, regression_name)
 
-    # generate image from pure noise
-    animation = gen.generate_animation(diffusion)
-    imgtools.animate_images(animation,False)
+    # # generate image from pure noise
+    # animation = gen.generate_animation(diffusion)
+    # imgtools.animate_images(animation,False, "animation.gif")
     # anim_normal, anim_predict = gen.double_generate(diffusion,regression)
     # imgtools.compare_grayscale_animations(anim_normal,anim_predict,"comparison.gif")
 
@@ -40,15 +40,17 @@ def main():
 
     # look for noise level, that human can recognize object for weights
     # test_image = get_image()
-    # noise_animation = blur.animate_noisy_image(test_image,time=140)
+    # noise_animation = blur.animate_noisy_image(test_image,time=500)
     # imgtools.animate_images(noise_animation,False)
 
     # do training MLP
-    # train_MLP(num_epochs=2000, layer_dim=128,
-    #           lr=1e-4, batch_size=128, tolerance=2000)
+    # train_MLP(num_epochs=2000, layer_dim=256,
+    #           lr=1e-5, batch_size=16, tolerance=2000)
 
     # do training diffusion model
-    # train_diffusion(num_epochs=2000,lr=1e-4,batch_size=128,tolerance=2000)
+    # train_diffusion(num_epochs=2000,lr=1e-3,batch_size=16,tolerance=2000)
+    test_power_training()
+    
 
 
     # do test
--- modules/train/ddpm.py
+++ modules/train/ddpm.py
@@ -1,13 +1,18 @@
 
 
 from tqdm.auto import tqdm
-from torch import nn, optim, no_grad, randint, clamp
+from torch import nn, optim, no_grad, randint, clamp, randn_like, arange
 from ..utils import constants as const
 from ..noise.diffusion import q_sample
 from ..models.model import Unet
 from ..models.IO import save_model, load_model
 from ..dataset.tools import prepare_dataset
 from ..images.transforms import transform
+
+# test
+from torch.utils.data import DataLoader
+from ..dataset.test import Diff_Single_Image_Dataset
+from ..dataset.init import dataset
 
 def train_diffusion(filename=None, num_epochs=None, lr=None, batch_size=None, tolerance=None):
     '''
@@ -38,12 +43,12 @@ def train_diffusion(filename=None, num_epochs=None, lr=None, batch_size=None, to
         batch_size=batch_size, transform=transform)
 
     # Define the loss function (Mean Squared Error) and the optimizer (e.g., SGD)
-    criterion = nn.MSELoss()
+    criterion = nn.SmoothL1Loss()
     # criterion = weighted_MSE_loss(const.default_MSE_weights)
 
     if lr is None:
         lr = const.default_learning_rate
-    optimizer = optim.AdamW(model.parameters(), lr=lr)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
 
     # load trained model
     old_epoch = 0
@@ -75,16 +80,16 @@ def train_diffusion(filename=None, num_epochs=None, lr=None, batch_size=None, to
             # t = record_time("move to device",t)
 
             # noisy images
-            # clamp [-1,1]
-            noise_samples = clamp(q_sample(image_samples, t_samples),min=-1,max=1)
+            noise = randn_like(image_samples)
+            noise_samples = q_sample(image_samples, t_samples, noise=noise)
             # t = record_time("generate q_samples",t)
 
             # Forward pass
-            predict_denoise = model(noise_samples, t_samples)
+            predict_noise = model(noise_samples, t_samples)
             # t = record_time("pass samples to model",t)
             
             # Compute the loss
-            loss = criterion(predict_denoise, noise_samples)
+            loss = criterion(predict_noise, noise)
             # t = record_time("compute loss",t)
 
             # Backpropagation and optimization
@@ -123,14 +128,15 @@ def train_diffusion(filename=None, num_epochs=None, lr=None, batch_size=None, to
                 const.default_device), t_samples.to(const.default_device)
 
             # noisy images
-            eval_noise_samples = clamp(q_sample(eval_samples, t_samples),min=-1,max=1)
+            noise = randn_like(eval_samples)
+            eval_noise_samples = q_sample(eval_samples, t_samples, noise=noise)
 
             # Forward pass
             with no_grad():
-                predict_denoise = model(eval_noise_samples, t_samples)
+                predict_noise = model(eval_noise_samples, t_samples)
 
             # Compute the loss
-            loss = criterion(predict_denoise, eval_noise_samples)
+            loss = criterion(predict_noise, noise)
 
             # accumulate loss
             test_loss += loss
@@ -168,4 +174,97 @@ def train_diffusion(filename=None, num_epochs=None, lr=None, batch_size=None, to
         previous_loss = avg_test_loss  # reset loss
         best_loss = min(avg_test_loss, best_loss)
 
+    print('training finished')
+
+def test_power_training():
+    # variables for training optimization
+    previous_loss = 1024.  # TODO: find better number
+    improvement_threshold = 0
+    no_improvement_count = 0
+    large_number = 1000000
+    tolerance = large_number
+
+    # init model
+    model = Unet(
+    dim=const.image_size,
+    channels=3,
+    dim_mults=(1, 2, 4,)
+    ).to(const.default_device)
+
+    # input 1 image
+    test_image = dataset['train']['img'][0]
+
+    # generate noise levels of image
+    train_dataset = Diff_Single_Image_Dataset(
+        image=test_image, transform=transform)
+    train_loader = DataLoader(
+        train_dataset, batch_size=16, shuffle=True)
+
+    # as batch, train with Adam on MSE
+
+    criterion = nn.MSELoss()
+    lr = 1e-4
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    previous_loss = 200.
+    # train
+    # train loop
+    for epoch in tqdm(range(large_number),desc=f"diffusion; lr: lr"):
+        # init previous loss
+        total_loss = 0.
+        avg_loss = 0.
+        for step, batch in enumerate(train_loader):
+            # move batch to device
+            batch = batch.to(const.default_device)
+            
+            t_samples = randint(0, const.timesteps, (len(batch),),
+                                device=const.default_device)
+            # noisy images
+            noise = randn_like(batch)
+            noise_samples = q_sample(batch, t_samples, noise=noise)
+
+            # forward noise
+            predict_noise = model(noise_samples, t_samples)
+            
+            # Compute the loss
+            loss = criterion(predict_noise, noise)
+
+            # Backpropagation and optimization
+            optimizer.zero_grad()
+            loss.backward()  # keeps track of outputs to go back to model
+            optimizer.step()
+
+            total_loss += loss
+            avg_loss = total_loss / (step+1)
+        # measure loss
+        if epoch % 10 == 0:
+            print(
+                f"{epoch} step trained. Loss: {avg_loss}")
+            # compare significance of training
+            # early stop, if after 5 epochs the test performance improves less than a certain threshold
+            # improvement defined as 100 % when reaching 0
+            # [0,1]: improvement; [0,-infty]: bad
+            improvement = 1. - (avg_loss / previous_loss)
+            print(
+                f"improvement: {improvement}, avg: {avg_loss}, prev:{previous_loss}")
+
+            # early stopping
+            if (improvement < improvement_threshold):
+                # no improvement
+                no_improvement_count += 1
+                print('No significant improvement')
+                if no_improvement_count >= tolerance:
+                    print(
+                        f'No significant improvement for {tolerance} epochs. Early stopping.')
+                    break
+
+            previous_loss = avg_loss  # reset loss
+            # no storing model
+            save_model(model=model, lr=lr)
+        # reach desired loss
+        if avg_loss <= 6.25e-7:
+            print("good job! saving and call it a day")
+            break
+
+    save_model(model=model, lr=lr)
     print('training finished')
