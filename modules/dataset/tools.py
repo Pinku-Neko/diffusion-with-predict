53af8d8b07480d6dde04c56daa70a70f2b639dd4
Pinku-Neko <henryiakvictor@gmail.com>
2023-11-14 12:29:37 +0100

    add drop_last to dataloader

--- modules/dataset/tools.py
+++ modules/dataset/tools.py
@@ -5,7 +5,7 @@ from modules.dataset.mydataset import Image_Dataset
 from torch.utils.data import DataLoader
 
 
-def prepare_dataset(batch_size, transform):
+def prepare_dataset(batch_size, transform, drop_last = None, num_workers = 0):
     '''
     use the predefined dataset to generate shuffled data loader with given batch size
     -precondition: the data set has 'train' and 'test' labels, whose labels are 'label' and 'image'
@@ -16,11 +16,11 @@ def prepare_dataset(batch_size, transform):
     # train data loader
     train_dataset = Image_Dataset(
         images=dataset['train']['img'], transform=transform)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=drop_last, num_workers=num_workers)
 
     # test data loader
     test_dataset = Image_Dataset(
         images=dataset['test']['img'], transform=transform)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, drop_last=drop_last, num_workers=num_workers)
 
     return train_loader, test_loader
